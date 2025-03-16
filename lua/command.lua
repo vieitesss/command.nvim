dff1c91221af53a1158bd0094c0b10fccd6284c3
Daniel Vieites <dvieitest@gmail.com>
2025-03-16 16:19:15 +0100

    feat: setup function and input command

--- lua/command.lua
+++ lua/command.lua
@@ -1,6 +1,21 @@
 local M = {}
 
-M.printHello = function()
+local COMMAND = ""
+
+M.setup = function ()
+    vim.api.nvim_create_user_command("CommandExecute", M.new_command, {})
+    vim.api.nvim_create_user_command("CommandRexecute", M.exec_command_again, {})
+
+    local opts = { silent = true, noremap = true }
+    vim.api.nvim_set_keymap("n", "<space>co", "<cmd>CommandExecute<cr>", opts)
+    vim.api.nvim_set_keymap("n", "<space>cr", "<cmd>CommandRexecute<cr>", opts)
+end
+
+M._update_command = function()
+    COMMAND = vim.fn.input("Command to execute: ")
+end
+
+M._exec_command = function()
     local buf = vim.api.nvim_create_buf(true, false)
     if buf == 0 then
         print("Error creating the buffer")
@@ -8,7 +23,7 @@ M.printHello = function()
     end
 
     local width = tonumber(vim.api.nvim_command_output("echo &columns"))
-    local height = math.floor(tonumber(vim.api.nvim_command_output("echo &lines")) * 0.2)
+    local height = math.floor(tonumber(vim.api.nvim_command_output("echo &lines")) * 0.25)
 
     local win = vim.api.nvim_open_win(buf, true, {
         width = width,
@@ -21,7 +36,7 @@ M.printHello = function()
         return
     end
 
-    local cmd = { "/usr/bin/env", "bash", "-c", "./script.sh dani" }
+    local cmd = { "/usr/bin/env", "bash", "-c", COMMAND }
 
     local chan = vim.fn.termopen(cmd)
     if not chan or chan <= 0 then
@@ -30,6 +45,19 @@ M.printHello = function()
     end
 end
 
-M.printHello()
+M.new_command = function()
+    M._update_command()
+    M._exec_command()
+end
+
+M.exec_command_again = function ()
+    if COMMAND == "" then
+        print("[ERROR] You have not executed a command before. Run `:ExecCommand`.")
+        return
+    end
+    M._exec_command()
+end
+
+M.setup()
 
 return M

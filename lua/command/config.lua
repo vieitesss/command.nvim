1bb5c7799951fac930ac8f7bf13c96171941bd8a
Daniel Vieites <dvieitest@gmail.com>
2025-09-03 20:49:22 +0200

    feat: ghost accept keymap

--- lua/command/config.lua
+++ lua/command/config.lua
@@ -25,7 +25,7 @@ local defaults = {
                 { '<C-f>', prompt_act.search },
                 { '<CR>', prompt_act.enter },
                 { '<C-d>', prompt_act.cancel },
-                { '<Right>', prompt_act.accept_ghost },
+                { '<C-e>', prompt_act.accept_ghost },
             },
             n = {
                 { '<Esc>', prompt_act.cancel }

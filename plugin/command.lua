10f4e4011c37c56a57d85fa40e45cbca95a54968
Daniel Vieites <dvieitest@gmail.com>
2025-12-07 13:30:00 +0100

    feat: add <Plug> mappings

--- plugin/command.lua
+++ plugin/command.lua
@@ -2,8 +2,19 @@ local ok, command = pcall(require, 'command')
 
 if not ok then
     vim.notify('[command.nvim] failed to load', vim.log.levels.ERROR)
+    return
 end
 
+-- Define <Plug> mappings for user customization
+vim.keymap.set({'n', 'i'}, '<Plug>(CommandExecute)', function()
+    command.execute()
+end, { desc = 'Open command prompt' })
+
+vim.keymap.set({'n', 'i'}, '<Plug>(CommandExecuteLast)', function()
+    command.execute_last()
+end, { desc = 'Execute last command' })
+
+-- Create user commands
 vim.api.nvim_create_user_command("CommandExecute", function()
     command.execute()
 end, {})
